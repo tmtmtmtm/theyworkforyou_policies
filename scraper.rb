8731885b483daddcbb2f5f2a699c9b7be3f6cd51
Tony Bowden <tony@tmtm.com>
2014-07-26 11:30:43 +0300

    initial scraper

--- scraper.rb
+++ scraper.rb
@@ -1,24 +1,55 @@
-# This is a template for a Ruby scraper on Morph (https://morph.io)
-# including some code snippets below that you should find helpful
 
-# require 'scraperwiki'
-# require 'mechanize'
-#
-# agent = Mechanize.new
-#
-# # Read in a page
-# page = agent.get("http://foo.com")
-#
-# # Find somehing on the page using css selectors
-# p page.at('div.content')
-#
-# # Write out to the sqlite database using scraperwiki library
-# ScraperWiki.save_sqlite(["name"], {"name" => "susan", "occupation" => "software developer"})
-#
-# # An arbitrary query against the database
-# ScraperWiki.select("* from data where 'name'='peter'")
+require 'scraperwiki'
+require 'open-uri'
+# require 'open-uri/cached'
+# OpenURI::Cache.cache_path = '/tmp/cache'
 
-# You don't have to do things with the Mechanize or ScraperWiki libraries. You can use whatever gems are installed
-# on Morph for Ruby (https://github.com/openaustralia/morph-docker-ruby/blob/master/Gemfile) and all that matters
-# is that your final data is written to an Sqlite database called data.sqlite in the current working directory which
-# has at least a table called data.
+@PAGE = 'https://raw.githubusercontent.com/mysociety/theyworkforyou/master/classes/Policies.php'
+
+def issue_from (line)
+  return unless m = line.match( /(\d+) => '(.*)',?\s?$/ )
+  return {
+    id: m[1],
+    text: m[2].gsub("\\'", "&rsquo;"),
+  }
+end
+
+def find_policy_list (page_text)
+  return page_text[/protected \$policies = array\((.*?)\);$/m, 1].split(/\n/).map { |line|
+    issue_from(line)
+  }.reject { |p| p.nil? }
+end
+
+def find_categories (page_text)
+  lines = page_text.split(/\n/).select { |line|
+    line if line =~ /private \$sets = array/ .. line =~ /\);/
+  }
+  lines.drop(1)
+end
+
+def scraped_policies
+  cats = {}
+  section = 'XXX'
+  page_text = open(@PAGE).read
+  find_categories(page_text).each do |line|
+    if line =~ /'([^']+)' => array/
+      section = $1
+    else
+      id = line[/(\d+)/, 1]
+      next if id.nil?
+      (cats[section] ||= []) << id
+    end
+  end
+
+  find_policy_list(page_text).map { |i|
+    {
+      id: i[:id],
+      text: i[:text],
+      categories: cats.select { |k,v| v.include? i[:id] }.keys.join(","),
+    }
+  }
+end
+
+ScraperWiki.save_sqlite([:id], scraped_policies)
+
+
